b14d59f5f8a66ce2bb49946175c87c0eeffdf255
skinitybysofia <skinitybysofia@gmail.com>
2025-06-18 18:52:45 +0100

    Uploading website

---
+++ send_email.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $to = "skinitybysofia@gmail.com";
+    $subject = "New Contact Form Message from Skinity Website";
+    
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+    
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+    
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo json_encode(['status' => 'success', 'message' => 'Message sent successfully']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Failed to send message']);
+    }
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+}
+?> 
